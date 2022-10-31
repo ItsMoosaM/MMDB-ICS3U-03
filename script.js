1a8bbe04a6f806193d4bb98a8e7145a69875b201
ItsMoosaM <moosamughal18@gmail.com>
2022-10-31 11:24:09 -0400

    Made API return videos and display first trailer

--- script.js
+++ script.js
@@ -23,16 +23,21 @@ const getMovie = async (selectedMovie) => {
     }
   });
 
+  const extraData = await getData(`https://api.themoviedb.org/3/movie/${movieData.data.results.at(0).id}`, {
+    params: {
+      api_key: "ba4adcc4706ed37650e0a813de11a08f",
+      append_to_response: "videos",
+    }
+  });
   console.log(movieData.data.results.at(0));
 
-
+  const trailer = document.getElementById('trailer');
   const movie_title = document.getElementById('movie_title');
   const poster = document.getElementById('poster');
 
   movie_title.innerHTML = `${movieData.data.results.at(0).title}`;
-
   poster.src = `https://image.tmdb.org/t/p/w500${movieData.data.results.at(0).poster_path}`
-
+  trailer.src = `https://www.youtube.com/embed/${extraData.data.videos.results.filter((video) => video.type === "Trailer").at(0).key}`
 
 }
 
