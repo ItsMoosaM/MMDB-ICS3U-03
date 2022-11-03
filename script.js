93b34e031ef938b845ef2d4cbd6af6ee9338e6df
ItsMoosaM <moosamughal18@gmail.com>
2022-11-03 10:18:46 -0400

    Displayed a list for each genre in an  array

--- script.js
+++ script.js
@@ -23,23 +23,43 @@ const getMovie = async (selectedMovie) => {
     }
   });
 
-  const extraData = await getData(`https://api.themoviedb.org/3/movie/${movieData.data.results.at(0).id}`, {
+  let movieDataResults = movieData.data.results.at(0);
+
+  const extraData = await getData(`https://api.themoviedb.org/3/movie/${movieDataResults.id}`, {
     params: {
       api_key: "ba4adcc4706ed37650e0a813de11a08f",
       append_to_response: "videos",
     }
   });
-  console.log(movieData.data.results.at(0));
 
+  let extraDataResults = extraData.data;
+
+  console.log(movieDataResults);
+  console.log(extraDataResults);
+
+  
+  
+  
   const trailer = document.getElementById('trailer');
   const movie_title = document.getElementById('movie_title');
   const overview = document.getElementById('overview');
   const poster = document.getElementById('poster');
+  const genres = document.getElementById('genres')
+  
+  genres.innerHTML = '';
 
-  movie_title.innerHTML = `${movieData.data.results.at(0).title}`;
-  overview.innerHTML = `${movieData.data.results.at(0).overview}`
-  poster.src = `https://image.tmdb.org/t/p/w500${movieData.data.results.at(0).poster_path}`
-  trailer.src = `https://www.youtube.com/embed/${extraData.data.videos.results.filter((video) => video.type === "Trailer").at(0).key}`
+  let genresInfo = extraDataResults.genres.forEach(element => {
+    let genresNames = element.name;
+    console.log(genresNames)
+    
+    genres.innerHTML += `<li> ${genresNames} </li>`
+  }); 
+
+  movie_title.innerHTML = `${movieDataResults.title}`;
+  overview.innerHTML = `${movieDataResults.overview}`
+  // genres.innerHTML = `${extraDataResults.genres.for(0,1,3,4).name}`
+  poster.src = `https://image.tmdb.org/t/p/w500${movieDataResults.poster_path}`
+  trailer.src = `https://www.youtube.com/embed/${extraDataResults.videos.results.filter((video) => video.type === "Trailer").at(0).key}`
 
 }
 
