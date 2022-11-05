4a50c3196bb8e26fbc94738b462ebebd3aec50bb
ItsMoosaM <moosamughal18@gmail.com>
2022-11-04 23:27:27 -0400

    Added more info, used CSS grid to make a layout, and made more CSS changes

--- script.js
+++ script.js
@@ -23,44 +23,60 @@ const getMovie = async (selectedMovie) => {
     }
   });
 
-  let movieDataResults = movieData.data.results.at(0);
+  let lessMovieDataResults = movieData.data.results.at(0);
 
-  const extraData = await getData(`https://api.themoviedb.org/3/movie/${movieDataResults.id}`, {
+  const extraData = await getData(`https://api.themoviedb.org/3/movie/${lessMovieDataResults.id}`, {
     params: {
       api_key: "ba4adcc4706ed37650e0a813de11a08f",
       append_to_response: "videos",
     }
   });
 
-  let extraDataResults = extraData.data;
+  let movieDataResults = extraData.data;
 
+  console.log(lessMovieDataResults);
   console.log(movieDataResults);
-  console.log(extraDataResults);
 
   
   
   
-  const trailer = document.getElementById('trailer');
   const movie_title = document.getElementById('movie_title');
-  const overview = document.getElementById('overview');
+  const tagline = document.getElementById('tagline');
   const poster = document.getElementById('poster');
-  const genres = document.getElementById('genres')
-  
-  genres.innerHTML = '';
+  const trailer = document.getElementById('trailer');
+  const overview = document.getElementById('overview');
+  const genreTitle = document.getElementById('genreTitle')
+  const genres = document.getElementById('genres');
+  const releaseDate = document.getElementById('releaseDate');
+  const productionCompany = document.getElementById('productionCompany');
+  const movieCollection = document.getElementById('movieCollection');
+  const backdropImg = document.getElementById('backdropImage');
+  const backdropImageLink = document.getElementById('backdropImageLink');
+  const website = document.getElementById('movieWebsite');
 
-  let genresInfo = extraDataResults.genres.forEach(element => {
+
+  
+  movie_title.innerHTML = `${movieDataResults.title}`;
+  tagline.innerHTML = `${movieDataResults.tagline}`;
+  poster.src = `https://image.tmdb.org/t/p/w500${movieDataResults.poster_path}`
+  trailer.src = `https://www.youtube.com/embed/${movieDataResults.videos.results.filter((video) => video.type === "Trailer").at(0).key}`
+  overview.innerHTML = `${movieDataResults.overview}`
+  
+  genreTitle.innerHTML = 'Genres';
+  genres.innerHTML = '';
+  let genresInfo = movieDataResults.genres.forEach(element => {
     let genresNames = element.name;
     console.log(genresNames)
-    
     genres.innerHTML += `<li> ${genresNames} </li>`
   }); 
 
-  movie_title.innerHTML = `${movieDataResults.title}`;
-  overview.innerHTML = `${movieDataResults.overview}`
-  // genres.innerHTML = `${extraDataResults.genres.for(0,1,3,4).name}`
-  poster.src = `https://image.tmdb.org/t/p/w500${movieDataResults.poster_path}`
-  trailer.src = `https://www.youtube.com/embed/${extraDataResults.videos.results.filter((video) => video.type === "Trailer").at(0).key}`
-
+  releaseDate.innerHTML = `Release Date: ${movieDataResults.release_date}`;
+  productionCompany.innerHTML = `Production Company: ${movieDataResults.production_companies[0].name}`;
+  backdropImg.src = `https://image.tmdb.org/t/p/w500${movieDataResults.backdrop_path}`
+  backdropImageLink.href = `${movieDataResults.homepage}`
+  website.innerHTML = `${movieDataResults.homepage}`
+  website.href = `${movieDataResults.homepage}`
+  movieCollection.innerHTML = `Collection: ${movieDataResults.belongs_to_collection.name}`;
 }
 
 
